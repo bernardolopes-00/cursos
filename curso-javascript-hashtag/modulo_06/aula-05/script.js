d6a4a6c0954ffb2afdb48a0652078bf50a789071
Bernardo Lopes <bernardolopesdev@gmail.com>
2025-06-17 18:00:02 -0300

    novo exercicio

--- curso-javascript-hashtag/modulo_06/aula-05/script.js
+++ curso-javascript-hashtag/modulo_06/aula-05/script.js
@@ -1,14 +1,20 @@
-const usuario = { nome: "Bernardo", idade: 14, time: "Atlético MG" };
-const mensagemDeBoasVindas1 = "Bem vindo, Torcedor Alvinegro!";
-const mensagemDeBoasVindas2 = "Bem vindo, Torcedor Palmeirense!";
+const usuario = { nome: 'Daniel', idade: 17, time: 'Sport' };
+const mensagemDeBoasVindas1 = 'Bem vindo, torcedor Tricolor!';
+const mensagemDeBoasVindas2 = 'Bem vindo, torcedor Rubro-Negro!';
+const mensagemDeBoasVindas3 = 'Bem vindo, torcedor Vascaíno!';
+const mensagemDeBoasVindasGeral = 'Bem vindo, amante do esporte!';
 
-if (usuario.time === "Atlético MG") {
-  console.log(mensagemDeBoasVindas1);
-} else {
-  console.log(mensagemDeBoasVindas2);
-}
+usuario.time === 'Fluminense' && usuario.idade >= 18
+  ? console.log(mensagemDeBoasVindas1)
+  : usuario.time === 'Flamengo' && usuario.idade >= 18
+  ? console.log(mensagemDeBoasVindas2)
+  : usuario.idade >= 18
+  ? console.log(mensagemDeBoasVindasGeral)
+  : console.log('Não vendemos ingresso para torcedores menores de 18 anos');
 
-const mensagemDeEscolha = 'Clique no setor para o qual deseja comprar seu ingresso!';
-const mensagemFinal = 'Divirta-se na Arena MRV!!!';
+const mensagemDeEscolha =
+  'Clique no setor para o qual deseja comprar seu ingresso!';
+const mensagemFinal = 'Divirta-se no Maracanã!!!';
 
 console.log(mensagemDeEscolha);
+console.log(mensagemFinal);
